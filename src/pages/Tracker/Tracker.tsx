cb8d7c62258bf101149506fd8a3d8b1af59e0e71
illiachumak <illia.chumak.2017@gmail.com>
2023-05-09 19:53:21 +0300

    Changes in tracker service

--- src/pages/Tracker/Tracker.tsx
+++ src/pages/Tracker/Tracker.tsx
@@ -1,43 +1,15 @@
-import React from 'react'
-
-import moment from 'moment';
-import { $api } from 'utils/axios';
-import { useAuth } from 'hooks/useAuth';
-
 import { Datepicker } from 'components/Calendar/Calendar';
 import { EmployeeCard } from 'components/EmployeeCard/EmployeeCard';
 import { CurrentDayCard } from 'components/CurrentDayCard/CurrentDayCard';
 
 
-function Tracker() {
-	const { user } = useAuth()
-	const [trackedTimeByDay, updateTrackedTimeByDay] = React.useState(0);
-	const getTrackedTimeByDay = async () => {
-		try {
-			if(!(activeDate instanceof Date)){
-				throw new Error('Not a date.')
-			}
-			const formatedDate = moment(activeDate).format().split('+')[0] + 'Z'
-			
-			const res = await $api.post<number>(`/tracker/day`,{
-				userId: user.id,
-				date: formatedDate,
-			})
-			updateTrackedTimeByDay(res.data)
-		} catch (e) {
-			if(e instanceof Error){
-				throw new Error(e.message);
-			}
-		}
-	}
 
-	const [activeDate, changeActiveDate] = React.useState<Date | (Date | null)[] | null>(new Date());
-	
+function Tracker() {
 	return (
 		<section className='flex mt-[100px]'>
-			<Datepicker changeActiveDate={changeActiveDate} activeDate={activeDate} />
-			<EmployeeCard activeDate={activeDate} getTrackedTimeByDay={getTrackedTimeByDay} />
-			<CurrentDayCard activeDate={activeDate} getTrackedTimeByDay={getTrackedTimeByDay} trackedTimeByDay={trackedTimeByDay} />
+			<Datepicker />
+			<EmployeeCard />
+			<CurrentDayCard />
    		</section>
 	);
 }
--- src/utils/redux/store.ts
+++ src/utils/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authSlice from './slices/authSlice'
+import trackerSlice from './slices/trackerSlice'
 
 export const store = configureStore({
 	reducer: {
-		auth: authSlice
+		auth: authSlice,
+		tracker: trackerSlice
 	},
 })
 
